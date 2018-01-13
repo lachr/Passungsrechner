331fb4a3566088d6f012df8fc0660831aa9e236c
André Lacher <andre.lacher@27degrees.ch>
2018-01-13 14:52:30 +0100

    saved fits design

--- src/savedFits.vue
+++ src/savedFits.vue
@@ -1,43 +1,46 @@
 <template>
 
 <div>
-    <div v-for="(item, index) in savedFits" class="flex">
-        <div>
-            <h4>Nennmass</h4>
-            {{ item.fit.basicSize }} mm
+    <div v-for="(item, index) in savedFits" class="inline-flex border-b">
+        <div class="p-2">
             <h4>{{ item.fitType | fitTypeDe }}</h4>
-            <span v-if="item.fitType != 'transition'">max. Spiel</span>
-            <span v-if="item.fitType == 'transition'">max. Übermass</span> 
-            {{ Math.abs(item.maxDiff) }}mm<br>
-            <span v-if="item.fitType == 'clearance'">min. Spiel</span>
-            <span v-if="item.fitType == 'interference'">max. Übermass</span>
-            <span v-if="item.fitType == 'transition'">min. Übermass</span> 
-            {{ Math.abs(item.minDiff) }}mm
+            {{ Math.abs(item.maxDiff) }}
+            <span class="text-grey-dark" v-if="item.fitType != 'transition'">max. Spiel</span>
+            <span class="text-grey-dark" v-if="item.fitType == 'transition'">max. Übermass</span> 
+            <br>
+            {{ Math.abs(item.minDiff) }}
+            <span class="text-grey-dark" v-if="item.fitType == 'clearance'">min. Spiel</span>
+            <span class="text-grey-dark" v-if="item.fitType == 'interference'">max. Übermass</span>
+            <span class="text-grey-dark" v-if="item.fitType == 'transition'">min. Übermass</span> 
         </div>
-        <div>
+        <div class="p-2">
             <h4>Bohrung</h4>
             <div class="flex">
-                <div>
-                    {{ item.fit.hole.upperDeviation }}mm<br>
-                    {{ item.fit.hole.lowerDeviation }}mm
-                </div>
-                <div>
-                    {{ item.selectedHole }}
-                </div>
-            </div>
-            <h4>Welle</h4>
-            <div class="flex">
-                <div>
-                    {{ item.fit.shaft.upperDeviation }}mm<br>
-                    {{ item.fit.shaft.lowerDeviation }}mm
-                </div>
-                <div>
-                    {{ item.selectedShaft }}
-                </div>
+                <span class="text-lg pt-2 pr-1">
+                    {{ item.fit.basicSize }}
+                    <span class="text-grey-dark">{{ item.selectedHole }}</span>
+                </span>
+                <span class="text-sm">
+                    {{ item.fit.hole.upperDeviation }}<br>
+                    {{ item.fit.hole.lowerDeviation }}
+                </span>
             </div>
         </div>
-        <div>
-            <h4 @click="savedFits.splice(index,1)">Entfernen</h4>
+        <div class="p-2">
+            <h4>Welle</h4>
+            <div class="flex">
+                <span class="text-lg pt-2 pr-1">
+                    {{ item.fit.basicSize }}
+                    <span class="text-grey-dark">{{ item.selectedShaft }}</span>
+                </span>
+                <span class="text-sm">
+                    {{ item.fit.shaft.upperDeviation }}<br>
+                    {{ item.fit.shaft.lowerDeviation }}
+                </span>
+            </div>
+        </div>
+        <div class="py-2 self-center">
+            <button @click="savedFits.splice(index,1)" class="border rounded border-black py-1 px-2 hover:bg-black hover:text-white">x</button>
         </div>
     </div>
 </div>
